aac18cef3b72776b35b750d92f7352215c02d4e7
Mike Daley <michaelcdaley@yahoo.com>
2020-03-21 08:00:04 -0700

    Morgan http request logging

--- server/config/morgan.js
+++ server/config/morgan.js
@@ -7,7 +7,7 @@ const morgan = require('morgan')
  * Custom morgan token to display the http request params, query, and body
  */
 morgan.token('body', function (req, res) { 
-  let message = 'Dude, request: '
+  let message = 'request: '
   if(req.params)  { message += `params= ${JSON.stringify(req.params)}, ` }
   if(req.query)   { message += `query= ${JSON.stringify(req.query)}, `  }
   if(req.body)    { message += `body= ${JSON.stringify(req.body)}` }
