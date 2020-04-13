260f06013acf385ae21b860c6bc94687238c8e56
Mike Daley <michaelcdaley@yahoo.com>
2020-04-13 08:29:56 -0700

    Added transactions-api create account tests

--- client/src/api/__tests__/transactions-api.test.js
+++ client/src/api/__tests__/transactions-api.test.js
@@ -6,7 +6,7 @@ import TransactionsAPI  from '../transactions-api'
 
 jest.mock('axios')      // https://jestjs.io/docs/en/mock-functions#mocking-modules
 
-// Mock transactoin data for an account
+// Mock transaction data for an account
 const transactionsData = [
   { 
     _id:          '1',
@@ -43,7 +43,91 @@ const transactionsData = [
 describe('Transactions API', () => {
   afterEach( () => {
     axiosMock.get.mockClear()
-    //* axiosMock.post.mockClear()
+    axiosMock.post.mockClear()
+  })
+
+  describe('create', () => {
+    it('Creates a debit transaction', async () => {
+      const accountId = transactionsData[0].accountId
+      const url       = `http://localhost:5000/api/v1/accounts/${accountId}/transactions`
+      const params    = {
+        date:         '2020-03-31T07:00:00.000Z',
+        description:  'Transaction One', 
+        category:     'Groceries', 
+        charge:       'debit', 
+        amount:       -100.00
+      }
+
+      // Add mock implementation to simulate a server error
+      axiosMock.post.mockImplementationOnce( (url, params) =>
+        Promise.resolve({ data: {...params, _id: '99', accountId: accountId, userId: 'Me'} }),
+      )
+
+      let {transaction} = await TransactionsAPI.create(accountId, params)
+
+      expect(transaction.date).toBe('2020-03-31T07:00:00.000Z')
+      expect(transaction.description).toMatch(/transaction on/i)
+      expect(transaction.category).toBe('Groceries')
+      expect(transaction.charge).toBe('debit')
+      expect(transaction.amount).toBe(-100)
+      expect(transaction.debit).toBe(-100)
+      expect(transaction.credit).toBe('')
+      expect(transaction._id).toBe('99')
+      expect(transaction.accountId).toBe(accountId)
+      expect(transaction.userId).toBe('Me')
+    })
+
+    it('Creates a credit transaction', async () => {
+      const accountId = transactionsData[0].accountId
+      const url       = `http://localhost:5000/api/v1/accounts/${accountId}/transactions`
+      const params    = {
+        date:         '2020-03-31T07:00:00.000Z',
+        description:  'Transaction Two', 
+        category:     'Salary', 
+        charge:       'credit', 
+        amount:       400.00
+      }
+
+      // Add mock implementation to simulate a server error
+      axiosMock.post.mockImplementationOnce( (url, params) =>
+        Promise.resolve({ data: {...params, _id: '99', accountId: accountId, userId: 'Me'} }),
+      )
+
+      let {transaction} = await TransactionsAPI.create(accountId, params)
+
+      expect(transaction.date).toBe('2020-03-31T07:00:00.000Z')
+      expect(transaction.description).toMatch(/transaction two/i)
+      expect(transaction.category).toBe(params.category)
+      expect(transaction.charge).toBe(params.charge)
+      expect(transaction.amount).toBe(params.amount)
+      expect(transaction.debit).toBe('')
+      expect(transaction.credit).toBe(params.amount)
+      expect(transaction._id).toBe('99')
+      expect(transaction.accountId).toBe(accountId)
+      expect(transaction.userId).toBe('Me')
+    })
+
+    it('Returns a server error', async () => {
+      const url     = 'http://localhost:5000/api/v1/accounts/xxx/transactions'
+      const params  = {}
+
+      const serverError = {
+        server: { code: 500, message: 'Unable to get your accounts' }
+      }
+
+      // Add mock implementation to simulate a server error
+      axiosMock.post.mockImplementationOnce((url, params) =>
+        Promise.reject(serverError),
+      )
+
+      try {
+        const transaction = await TransactionsAPI.create(params)
+      }
+      catch(error) {
+        expect(error.server.code).toBe(500)
+        expect(error.server.message).toMatch(/Unable to connect to the server/)
+      }
+    })
   })
 
   /**
